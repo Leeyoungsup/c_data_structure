25b221ca087116a1c28fe5d923e46d97a4c508fb
leeyoungsup <58593829+Leeyoungsup@users.noreply.github.com>
2020-04-01 00:10:46 +0900

    list_data_structure_20.04.01

--- list.cpp
+++ list.cpp
@@ -3,6 +3,7 @@
 #include<malloc.h>
 void search(int *list);
 int* insert(int *list,int value,int location);
+int* Delete(int *list,int location);
 int main(){
     int *list;
     list=(int*)malloc(6*sizeof(int));
@@ -17,8 +18,11 @@ int main(){
 //  free(list);
 //  printf("%d\n",sizeof(list));
 //  search(list);
-    list=insert(list,7,11);
-    search(list); 
+    list=insert(list,7,6);
+    search(list);
+    printf("\n"); 
+    list=Delete(list,2);
+    search(list);
     return 0;
 }
 void search(int *list){
@@ -48,3 +52,26 @@ int* insert(int* list,int value,int location){
     }
 
 }
+int* Delete(int* list,int location){
+    int list_size=malloc_usable_size(list);
+    int list_length=list_size/4;
+    if(list_length-1<location){
+        printf("The requested locations is out of range");
+    }
+    else{
+        int *list1;
+        list1=(int*)malloc((list_length-1)*sizeof(int));
+        for(int i=0;i<list_length-1;i++){
+            if(i>=location){
+                list1[i]=list[i+1];
+            }
+            else{
+                list1[i]=list[i];
+            }
+        }
+        free(list);
+        return list1;
+    }
+
+}
+
