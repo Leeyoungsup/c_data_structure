861f65557234f450098b69b5ddbef4a4aa587c73
leeyoungsup <58593829+Leeyoungsup@users.noreply.github.com>
2020-03-31 23:50:45 +0900

    list_data_structure_20.03.31

--- list.cpp
+++ list.cpp
@@ -2,7 +2,7 @@
 #include<stdlib.h>
 #include<malloc.h>
 void search(int *list);
-int insert(int *list,int value,int location);
+int* insert(int *list,int value,int location);
 int main(){
     int *list;
     list=(int*)malloc(6*sizeof(int));
@@ -13,11 +13,12 @@ int main(){
     list[4]=5;
     list[5]=6;
     printf("%d\n",malloc_usable_size(list));
-    search(list);
 //  memory deallocation
 //  free(list);
 //  printf("%d\n",sizeof(list));
 //  search(list);
+    list=insert(list,7,11);
+    search(list); 
     return 0;
 }
 void search(int *list){
@@ -27,9 +28,23 @@ void search(int *list){
         printf("list[%d]=%d\n",i,list[i]);
     }
 }
-int insert(int *list,int value,int location){
+int* insert(int* list,int value,int location){
     int list_size=malloc_usable_size(list);
     int list_length=list_size/4;
-    int *list1;
+    if(list_length-1<location){
+        int *list1;
+        list1=(int*)malloc((location+1)*sizeof(int));
+        printf("%d\n",malloc_usable_size(list1));
+        for(int i=0;i<list_length;i++){
+            list1[i]=list[i];
+        }
+        free(list);
+        list1[location]=value;
+        return list1;
+    }
+    else{
+        list[location]=value;
+        return list;
+    }
 
 }
